08f27e50c6e5b847a473f328806724cf34650e1f
xyzmerencio <vitormdluzz2003@outlook.com>
2024-09-18 09:52:20 -0300

    aula 24

---
+++ api-front/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrincipalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ api-front/src/app/principal/principal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrl: './principal.component.css'
+})
+export class PrincipalComponent {
+
+  // Variável para visibilidade dos botões
+  btnCadastro:boolean = true;
+}
